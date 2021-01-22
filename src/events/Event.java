fa28320faf4fe4db435152ae064b5237b229950e
Luke Hanna <Luke.m.hanna@gmail.com>
2021-01-22 01:57:12 -0500

    Commented new Event class

--- src/events/Event.java
+++ src/events/Event.java
@@ -2,44 +2,44 @@ package events;
 
 import gui.GUI;
 
+// Superclass for all game events
 public abstract class Event {
 
-	private double length;
-	private double progress;
+	private double length;		// Length of event
+	private double progress;	// Duration since start of event
 	
-	private long start;
-	private long end;
-	
-	public Event(double length) {
-		this.length = length;
-		progress = 0;
+	// Constructor
+	public Event(double length) { 
+		this.length = length; // Set the length to the given parameter
+		progress = 0; // Set progress to 0
 
 
 	}
 	
+	// Update function gets called repeatedly
 	public Boolean update(double timeSince) {
-		progress += timeSince;
+		progress += timeSince; // Add time since last frame to progress
 		
-		eventManage(timeSince);
+		eventManage(timeSince); // call the event manage method (Subclasses are required to implement this)
 		
-		if (progress >= length) { // If more time has gone on then the length of 
-			end();
-			return false;
+		if (progress >= length) { // If our progress is the same or longer then the desired length of event
+			end(); // Call the events end() function
+			return false; // Return false so the eventManager knows the event is over
 		}
-		else {
-			return true;
+		else { // Otherwise, if the event is still running
+			return true; // Return true
 		}
 	}
-	
-	
-	public abstract void eventManage(double timeSince);
-	
 	
 	// Private function gets called before the event gets set null
-	private void end() {
+	private void end() { // Handles all the clean up all events have before ending
 		eventEnd();
 	}
 	
+	// Required abstract method that manages the event each frame
+	public abstract void eventManage(double timeSince);
+	
+	// Required abstract method that is called to end the event
 	public abstract void eventEnd();
 	
 	
