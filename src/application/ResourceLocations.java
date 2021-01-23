43486018a3b5a106065b7984df4be4e5a24b1e63
Luke Hanna <Luke.m.hanna@gmail.com>
2021-01-23 01:11:17 -0500

    Added a static class with final values for path values, making it easy to adjust images

---
+++ src/application/ResourceLocations.java
@@ -0,0 +1,11 @@
+package application;
+
+public class ResourceLocations {
+	// Sound files
+	static final String fireSound = "./Resources/Sounds/Effects/laser.wav";
+	
+	// Images
+	static final String fighterImage = "";
+	static final String protectorImage = "";
+	static final String planetImage = "";
+}
