ceecc695abb28f4643804dd3b0c0cd911f76ce77
Luke Hanna <Luke.m.hanna@gmail.com>
2021-01-27 23:36:50 -0500

    Fixed sizing on equipment pane

--- src/gui/EquippedGUI.java
+++ src/gui/EquippedGUI.java
@@ -4,11 +4,15 @@ import java.util.LinkedList;
 
 import application.GameObjects;
 import application.Player;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
 public class EquippedGUI {
 
 	private static EquippedGUI eqgui = null;
+	
+	private int width = 600;
+	private int height = 300;
 	
 	private  EquippedGUI() {
 		
@@ -24,13 +28,15 @@ public class EquippedGUI {
 	public void draw() {
 		LinkedList<Player> players = GameObjects.get().players();
 		boolean active = false;
-		for (Player p : player) {
+		for (Player p : players) {
 			if (p.isEquipOpen())
 				active = true;
 		}
-		
-		GraphicsContext gc = GameObjects.get().gameCanvas().getGraphicsContext2D();
-		gc.fillRect(0, 0, 50, 50);
+	
+		Canvas canvas = GameObjects.get().gameCanvas();
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+	
+		gc.fillRect(canvas.getWidth()/2-width/2, canvas.getHeight()/2-height/2, width, height);
 		
 	}
 	
