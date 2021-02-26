3bd66eb5f8992ee4e3ca8dbbabdfe837aef888fe
Luke Hanna <Luke.m.hanna@gmail.com>
2021-02-26 09:21:00 +0400

    Added Java Documentation for the incomplete Tanker Class

--- src/ships/Tanker.java
+++ src/ships/Tanker.java
@@ -2,8 +2,20 @@ package ships;
 
 import inventory.Itemizable;
 
+/**
+ * TODO: Flesh out the Tanker class ship further
+ * 
+ * @author Luke Hanna (Github.com/JustAPyro)
+ * @version 1.0
+ */
 public class Tanker extends Ship {
 
+	
+	/**
+	 * This returns True if the passed item is compatible to stack with this class object in shop menus and inventory.
+	 * 
+	 * @param item The item you want to know if this ship will stack with in shop & Inventory.
+	 */
 	@Override
 	public boolean stacksWith(Itemizable item) {
 		if (item.getClass().equals(this.getClass()))
@@ -12,6 +24,11 @@ public class Tanker extends Ship {
 			return false;
 	}
 
+	/**
+	 * TODO: Fill out description for the Ship class.
+	 * 
+	 * This returns a custom description for the Ship class vessel.
+	 */
 	@Override
 	public String getDescription() {
 		// TODO Auto-generated method stub
