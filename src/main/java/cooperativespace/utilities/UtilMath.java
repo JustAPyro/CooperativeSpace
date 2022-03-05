fd0add6cba3020effbef18614c6d9e5a46b09716
Luke Hanna <luke.m.hanna@gmail.com>
2022-03-05 15:35:23 -0500

    Added a UtilMath class with wrappers for trig functions that will convert in/out of radians

---
+++ src/main/java/cooperativespace/utilities/UtilMath.java
@@ -0,0 +1,13 @@
+package cooperativespace.utilities;
+
+public class UtilMath {
+
+    public static double sin(double input) {
+        return Math.toDegrees(Math.sin(Math.toRadians(input)));
+    }
+
+    public static double cos(double input) {
+        return Math.toDegrees(Math.cos(Math.toRadians(input)));
+    }
+
+}
