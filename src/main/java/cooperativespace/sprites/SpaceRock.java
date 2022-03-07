5b090e962791f6507f630609fab325621634c5a0
Luke Hanna <luke.m.hanna@gmail.com>
2022-03-07 17:49:58 -0500

    Implemented load() and draw() methods of SpaceRock and added some resources

--- src/main/java/cooperativespace/sprites/SpaceRock.java
+++ src/main/java/cooperativespace/sprites/SpaceRock.java
@@ -1,6 +1,29 @@
 package cooperativespace.sprites;
 
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+
 public class SpaceRock extends Sprite{
 
+    Image image;
 
+    @Override
+    public void load() {
+        image = new Image("C:\\Users\\Luke\\Downloads\\CooperativeSpace-primary\\CooperativeSpace-primary\\src\\main\\resources\\images\\rock.png");
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.getGraphicsContext2D().drawImage(image, getXPosition(), getYPosition());
+    }
+
+    @Override
+    public byte[] pack() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void unpack(byte[] playerPacket) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
