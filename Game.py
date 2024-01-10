ca9ad295bbe4dbdf69dde80eb0705728af680c33
Luke Hanna <Luke.m.hanna@gmail.com>
2024-01-10 09:50:13 -0500

    Reinvented movement

--- Game.py
+++ Game.py
@@ -7,29 +7,33 @@ class Player:
     def __init__(self):
         # Load and scale the asset for the players ship
         self.img = pygame.image.load('resources/images/ships/destroyer.png')
-        self.img = pygame.transform.smoothscale_by(self.img, 0.35)
+        self.img = pygame.transform.smoothscale_by(self.img, 0.2)
 
         # Position variables
-        self.velocity = 0
         self.position = [0, 0]
         self.heading = 0
 
+        # Acceleration and x/y velocity
+        self.acceleration = 0.25
+        self.velocity = [0, 0]
+
+
+
     def update(self, inputs):
-        if inputs[0]:
-            self.velocity += 1
-        if inputs[1]:
+        if inputs[0]:  # W
+            self.velocity[0] += math.sin(math.radians(self.heading)) * self.acceleration
+            self.velocity[1] += math.cos(math.radians(self.heading)) * -self.acceleration
+        if inputs[1]:  # A
             self.heading -= 1
-        if inputs[2]:
-            self.position[1] += 1
-        if inputs[3]:
+        if inputs[2]:  # S
+            self.velocity[0] -= math.sin(math.radians(self.heading)) * self.acceleration
+            self.velocity[1] -= math.cos(math.radians(self.heading)) * -self.acceleration
+        if inputs[3]:  # D
             self.heading += 1
 
-        if self.velocity != 0:
-            self.position = (
-                int(math.sin(90-self.heading) / self.velocity),
-                int(math.cos(90-self.heading) / self.velocity)
-            )
+        self.position[0] += self.velocity[0]
+        self.position[1] += self.velocity[1]
 
     def draw(self, screen, pos, heading):
-        tranformed = pygame.transform.rotate(self.img, heading*-1)
+        tranformed = pygame.transform.rotate(self.img, (heading * -1) + 90)
         screen.blit(tranformed, pos)
